9ec0395312fc582ae8a1ddfc4fd7f92abf22f41f
Erik <1097392509@qq.com>
2019-01-22 12:21:55 +0800

    添加备份多个数据库

--- shell/src/mysql/mysql_backup.sh
+++ shell/src/mysql/mysql_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################mysql 数据库备份脚本##############
-#	添加定时任务，每晚2点钟执行                     
+#       添加定时任务，每晚2点钟执行                     
 #   crontab -e                                  
 #   0 2 * * * sh /root/mysql_backup.sh            
 #                                                
@@ -13,10 +13,17 @@
 #数据库用户
 declare -r mysql_user='root'
 #数据库密码
-declare -r mysql_password='password'
-#要备份的数据库
-declare -r database_name='clouddb'
+declare -r mysql_password='Dahyper2018@123!'
+#要备份的数据库 数组
+declare -a database_array=('edb_device' 'edb_pay' 'eqplay_agent' 'eqplay_auth' 'eqplay_zipkin' 'test')
 #格式化日期
 declare -r today=`date -d today +"%Y-%m-%d"`
+#备份路径
+declare -r back_dir='/root/mysql_backup'
 
-mysqldump -u${mysql_user} -p${mysql_password} ${database_name} > ${database_name}-${today}.sql
+for database_name in ${database_array[@]}
+do
+        echo "start backup   $database_name "
+        mysqldump -u${mysql_user} -p${mysql_password} ${database_name} >${back_dir}/${database_name}-${today}.sql 2> /dev/null
+done
+
